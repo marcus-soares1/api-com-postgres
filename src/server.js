a3c1833caa8b2d93ee7150718aa63147bea41520
Marcus Soares <marcus.silva.soares1@gmail.com>
2025-04-28 19:08:53 -0300

    Rotas de rotas

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const router = require("./routes")
 const app = express()
 
 app.use(express.json())
-app.use("/api",router)
+app.use("/api", router)
 
 const PORT = 3000
 app.listen(PORT, ()=>console.log(`http://localhost:${PORT}/`))
