fb75f8aeb64318dde7170c0afec655959a05bfc0
Marcus Soares <marcus.silva.soares1@gmail.com>
2025-05-02 10:59:25 -0300

    Criação das funções da classe Customers

--- src/models/Customers.js
+++ src/models/Customers.js
@@ -3,8 +3,8 @@ const { query } = require("../database/index")
 class Customers{
     constructor(customerRow)
     {
-        this.name = atributtes.name
-        this.email = atributtes.email
+        this.name = customerRow.name
+        this.email = customerRow.email
     }
 
     static async findAll()
@@ -23,19 +23,33 @@ class Customers{
 
     static async create(name, email)
     {
-        const result = await query()
+        const result = await query(`INSERT INTO customers (name, email)
+            VALUES($1, $2) RETURNING *`, [name, email])
+        
+        return new Customers(result.rows[0])
+        
 
     }
 
     static async update(id, atributtes)
     {
+        const { rows } = await query(`SELECT * FROM customers WHERE id = $1`, [id])
+        if(!rows[0]) return null
+        
+        const result = await query(`UPDATE customers SET
+                name = $1,
+                email = $2
+            WHERE id = $3
+            RETURNING *`, [atributtes.name, atributtes.email, id])
 
+        return new Customers(result.rows[0])
 
     }
 
     static async deleteById(id)
     {
-
+        await query(`DELETE FROM products WHERE id = $1`, [id])
+        return { message: `Product deleted sucessfully.` }
 
     }
 }
