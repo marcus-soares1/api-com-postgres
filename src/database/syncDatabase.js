73762dc87122f91555ea28bfa525667e928967fa
Marcus Soares <marcus.silva.soares1@gmail.com>
2025-05-05 17:29:51 -0300

    Criação da tabela orders

--- src/database/syncDatabase.js
+++ src/database/syncDatabase.js
@@ -11,15 +11,43 @@ async function syncDatabase(){
           created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
           updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
           is_active BOOLEAN DEFAULT TRUE
-        );
-        CREATE TABLE IF NOT EXISTS customers (
-          id SERIAL PRIMARY KEY,
-          name VARCHAR(255) NOT NULL,
-          email VARCHAR(255) NOT NULL
-        )
-      `);
+        );`);
     console.log('Created "products" table.');
+
+    await query(`
+      CREATE TABLE IF NOT EXISTS customers (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        email VARCHAR(255) NOT NULL
+      );
+    `)
     console.log('Created "customers" table.');
+    
+    await query(`
+      CREATE TABLE IF NOT EXISTS orders(
+        id SERIAL PRIMARY KEY,
+        customer_id INT REFERENCES customers (id),
+        total DECIMAL(10, 2) NOT NULL DEFAULT (0),
+        current_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+      )
+    `)
+    console.log('Created "orders" table.');
+
+    await query(`
+      CREATE TABLE IF NOT EXISTS order_products(
+        order_id INT, 
+        product_id INT,
+        quantity INT NOT NULL DEFAULT 1,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        PRIMARY KEY (order_id, product_id),
+        FOREIGN KEY (order_id) REFERENCES orders (id),
+        FOREIGN KEY (product_id) REFERENCES products (id)
+      )  
+    `)
+    console.log('Created "orders_products" table.');
+
     process.exit(1);
 }
 
